865e9370748c138212eafcb8502234adc1ea4e77
idiotion <idiotion@hotmail.com>
2017-12-29 16:22:53 +0800

    后端数据服务框架
    后端数据服务框架

---
+++ rest_api/snippets/urls.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+from django.conf.urls import url ,include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register(r'snippets',views.SnippetViewSet)
+router.register(r'users' ,views.UserViewSet)
+
+urlpatterns = [
+    url(r'^',include(router.urls)),
+    url(r'^api-auth/',include('rest_framework.urls',namespace='rest_framework')),
+]
+
+# urlpatterns = format_suffix_patterns([
+#     url(r'^$',views.api_root),
+#
+#     url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+#     # url(r'^tasks/$', views.TaskList.as_view(), name='task_list'),
+#     # url(r'^tasks/$', views.TaskListCreate.as_view(), name='task_list'),
+#     url(r'^snippets/(?P<pk>[0-9]+)$', views.SnippetDetail.as_view(), name='snippet-detail'),
+#     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(),name='snippet-highlight'),
+#
+#     url(r'^users/$',views.UserList.as_view() ,name='user-list'),
+#     url(r'^users/(?P<pk>[0-9]+)/$',views.UserDetail.as_view() ,name='user-detail'),
+#
+# ])
+
+# urlpatterns += [
+#     url(r'^api-auth/', include('rest_framework.urls',
+#     namespace='rest_framework')),
+# ]
---
+++ rest_api/task/serializers.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ('id', 'title', 'description', 'completed', 'create_date')
